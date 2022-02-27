2272eb8103f8255cc41db6fcf3ec05b652a524bd
Alexandr Kotlyarov <egold92@yandex.ru>
2022-02-27 13:28:37 +0700

    Initial Commit

---
+++ OtusApp/OtusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusAppApp.swift
+//  OtusApp
+//
+//  Created by Александр Котляров on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct OtusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
